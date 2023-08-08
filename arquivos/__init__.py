9d96b5dec01b30bf75c5110d4c5f3dd4c423b2f9
Jhonata Batista <88687538+jhownny@users.noreply.github.com>
2023-08-08 08:39:54 -0300

    first commit

---
+++ arquivos/__init__.py
@@ -0,0 +1,106 @@
+
+
+def ArqExiste(nome):
+    
+    """
+    ArqExiste → Verifica a existencia do arquivo.
+    
+    Parâmetros:
+        - Caminho do arquivo
+        
+    Retorna:
+        - Retornara True caso o arquivo exista.
+        - Retornara Falso caso o arquivo não exista.
+        
+    """
+    
+    try:
+        a = open(nome, 'rt')
+        a.close()
+    except FileNotFoundError:
+        return False
+    else: 
+        return True
+    
+    
+def CriarArq(nome):
+    
+    """
+    CriarArq → Cria um arquivo.
+    
+    Parâmetros:
+        - Caminho do arquivo a ser adicionado    
+
+    Retorna:
+        - Retorna mensagem de sucesso caso consiga criar o arquivo.
+        - Retorna mensagem de erro caso não consiga criar o arquivo.
+        
+    """
+    
+    try:
+        a = open(nome, 'wt+')
+        a.close()
+    except:
+        print('Ocorreu um ERRO ao criar o arquivo')
+    else:
+        print(f'Aquivo {nome} criado com sucesso!')
+        
+
+def LerArq(nome):
+    
+    """
+    LerArq → ler um arquivo.
+    
+    Parâmetros:
+        - Caminho do arquivo a ser lido   
+
+    Retorna:
+        - Retorna mensagem de sucesso caso consiga criar o arquivo.
+        - Retorna mensagem de erro caso não consiga criar o arquivo.
+        
+    """
+    
+    try:
+        a = open(nome, 'rt')
+    except:
+        print('ERRO ao ler o arquivo')
+    else:
+        linhazinha()
+        LetraCor('      PESSOAS CADASTRADAS',3,1)
+        linhazinha()
+        for linha in a:
+            dado = linha.split(';')
+            dado[1] = dado[1].replace('\n','')
+            print(f'{dado[0]:<30}{dado[1]:>3} anos')
+    finally:
+        a.close()
+    
+
+def cadastrar(arq, nome='desconhecido', idade= 0):
+    
+    """
+    CADASTRAR → Cadastrar pessoas.
+    
+    Parâmetros:
+        - Caminho do arquivo a que será inserido o cadastro.  
+        - Nome a ser cadastrado.
+        - Idade a ser cadastrada.
+
+    Retorna:
+        - Retorna mensagem de sucesso caso consiga cadastrar.
+        - Retorna mensagem de erro caso não consiga cadastrar.
+        
+    """
+    
+    try:
+        a = open(arq,'at')
+    except:
+        print('Ocorreu um erro na abertura do aquivo!')
+    else:
+        try:
+            a.write(f'{nome.title()};{idade}')
+        except:
+            print('Ocorreu um erro na escritura do arquivo!')
+        else:
+            print(f'Novo registro de {nome} adicionado.\n')
+            a.close()
---
+++ dados/__init__.py
@@ -0,0 +1,56 @@
+def escolha(num):
+    
+    """
+    ESCOLHA -> Requere ao usuario a colocação de qualquer valor, mas só será aceito o valor 1,2 e 3 para as devidas respostas.
+    
+    Parâmetros:
+        - Colocação de Números para entrega das escolhas aceitanto apenas o numero 1,2 e 3 senão ocorrera o loop.
+        
+    Retorna:
+        - Retornara o valor caso o número inserido seja igual a 1, 2 e 3.
+        - mensagem de erro exigindo correção caso o número inserido seja diferente de 1, 2 e 3.
+        
+   """
+    
+    while True:
+        try: 
+            n = int(input(num))    
+        except (ValueError, TypeError):
+            print('\033[1;31mPor Favor digite um valor valido sugerido acima\033[m')
+            continue
+        except KeyboardInterrupt:
+            print('\033[1;31m\nVocê não digitou ou deixou em branco!\033[m')
+            return 3
+        else:
+            if n == 1 or n == 2 or n == 3:
+                return n
+            else:
+                print('\033[1;31mPor Favor digite um valor valido sugerido acima\033[m')
+                continue
+
+
+def LeiaInt(msg):
+    
+    """
+    LeiaInt -> Requere ao usuario a colocação de qualquer valor, mas só será aceito o valor que for numerico do tipo Int.
+
+    Parâmetros:
+        - Colocação de qualquer elemento, mas apenas será permitido aquele que for numerico do tipo Int.
+
+    Retorna:
+        - Retornara o valor que for numerico do tipo Int;
+        - mensagem de erro exigindo correção.
+        
+    """
+
+    while True:
+        try: 
+            n = int(input(msg))
+        except (ValueError, TypeError):
+            print(f'\033[1;31m ERRO, o Valor Digitado não se enquadra ao valor Requirido!\033[m')
+            continue
+        except KeyboardInterrupt:
+            print(f'\033[1;31m\nVocê não digitou ou deixou em branco!\033[m')
+            return 0
+        else:
+            return n  
