ce75dcaf12574443dda6a0d6197e1f31de4d787f
Jhonata Batista <88687538+jhownny@users.noreply.github.com>
2023-08-09 10:34:54 -0300

    Link com arquivo CSV - 09/08 Desktop

--- principal.py
+++ principal.py
@@ -1,5 +1,6 @@
 import PySimpleGUI as sg
 import random
+import csv
 import pandas as pd
 from arquivos import *
 from dados import *
@@ -11,41 +12,32 @@ if not ArqExiste(path):
 
 
 #(Escolhendo Aleatorio Filme - Dicionario)
-eafd = {'1960':['The Great Escape', 'What Ever Happened to Baby Jane?', 'For a Few Dollars More', 'Night of the Living Dead', 'Psycho', 'Harakiri', 'The Good, the Bad and the Ugly', 'Eyes Without a Face']
-    , '1970':['Alien', 'Blazing Saddles', 'Taxi Driver', 'The Godfather', 'Stalker', 'Jaws', 'The Exorcist', 'A Clockwork Orange']
-    , '1980':['Beetlejuice', 'An American Werewolf in London', 'The Thing', 'Scarface', 'The Color Purple', 'The Shining', 'Apocalypse Now', 'Once Upon a Time in America']
-    , '1990':['Malcolm X', 'Se7en', 'Heat', 'The Devils Advocate', 'Perfect Blue', 'Misery', 'Schindlers List', 'GoodFellas']
-    , '2000':['Office Space', 'No Country for Old Men', 'Watchmen', 'Who Am I', 'The Conjuring', 'Into the Wild', 'The Imitation Game', 'Get Out']}
 
-# Escolhendo uma chave aleatória do dicionário
-Ano_Aleatorio = random.choice(list(eafd.keys()))
+with open('arquivos/Filmes.csv','r') as arquivo:
+    leitor = csv.reader(arquivo)
+    ano = list(leitor)
+    ql = len(ano)
+    fl = random.randrange(1, ql-1)
+    al = random.randrange(0, 4)
 
-# Escolhendo um conteúdo aleatório da lista correspondente à chave aleatória
-Filme_Aleatorio = random.choice(eafd[Ano_Aleatorio])
-
+#eafd = {'1960':['The Great Escape', 'What Ever Happened to Baby Jane?', 'For a Few Dollars More', 'Night of the Living Dead', 'Psycho', 'Harakiri', 'The Good, the Bad and the Ugly', 'Eyes Without a Face']
+#    , '1970':['Alien', 'Blazing Saddles', 'Taxi Driver', 'The Godfather', 'Stalker', 'Jaws', 'The Exorcist', 'A Clockwork Orange']
+#    , '1980':['Beetlejuice', 'An American Werewolf in London', 'The Thing', 'Scarface', 'The Color Purple', 'The Shining', 'Apocalypse Now', 'Once Upon a Time in America']
+#    , '1990':['Malcolm X', 'Se7en', 'Heat', 'The Devils Advocate', 'Perfect Blue', 'Misery', 'Schindlers List', 'GoodFellas']
+#    , '2000':['Office Space', 'No Country for Old Men', 'Watchmen', 'Who Am I', 'The Conjuring', 'Into the Wild', 'The Imitation Game', 'Get Out']}
 
 #layout
 sg.theme('LightBrown13')
 layout = [
-    [sg.Text(f'O Ano escolhido foi {Ano_Aleatorio} e o Filme escolhido foi {Filme_Aleatorio}')],
-    [sg.Button('Aleatorizar')]
+    [sg.Text(f'O Ano escolhido foi {ano[0][al]} e o Filme escolhido foi {ano[fl][al]}')]
 ]
+
 # Janela
 janela = sg.Window('Escolhendo Filmes', layout)
+
 # Ler os Eventos
 while True:
     eventos, valores = janela.read()
     if eventos == sg.WINDOW_CLOSED:
         break
-    elif eventos == 'Aleatorizar':
-        
-        Ano_Aleatorio = random.choice(list(eafd.keys()))
-        Filme_Aleatorio = random.choice(eafd[Ano_Aleatorio])
-        # layout
-        sg.theme('LightBrown13')
-        layout = [
-            [sg.Text(f'O Ano escolhido foi {Ano_Aleatorio} e o Filme escolhido foi {Filme_Aleatorio}')],
-            [sg.Button('Aleatorizar')]
-        ]
-        # Janela
-        janela = sg.Window('Escolhendo Filmes', layout)
+
--- principalTeste.py
+++ principalTeste.py
@@ -1,7 +1,11 @@
 import csv
+from random import randrange
 
 with open('arquivos/Filmes.csv','r') as arquivo:
     leitor = csv.reader(arquivo)
-    dados = list(leitor)
-
-    print(dados)
+    ano = list(leitor)
+    ql = len(ano)
+    fl = randrange(1, ql-1)
+    al = randrange(0, 4)
+    
+    print(f'O ano escolhido foi \033[1;32m{ano[0][al]}\033[m e o filme \033[1;36m{ano[fl][al]}\033[m.')
