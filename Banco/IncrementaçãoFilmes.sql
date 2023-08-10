9613d3049f68344aaa97f09eb36ed7806ce99e56
Jhonata Batista <88687538+jhownny@users.noreply.github.com>
2023-08-10 11:02:08 -0300

    Tentativa de Inserção de Codigo para visualização de parâmetros cadastrados no banco - 10/08 Desktop

---
+++ Banco/IncrementaçãoFilmes.sql
@@ -0,0 +1,47 @@
+insert into cinema values
+(default,'1960', 'The Great Escape'),
+(default,'1960', 'What Ever Happened to Baby Jane?'),
+(default,'1960', 'For a Few Dollars More'),
+(default,'1960', 'Night of the Living Dead'),
+(default,'1960', 'Psycho'),
+(default,'1960', 'Harakiri'),
+(default,'1960', 'The Good, the Bad and the Ugly'),
+(default,'1960', 'Eyes Without a Face'),
+
+(default,'1970', 'Alien'),
+(default,'1970', 'Blazing Saddles'),
+(default,'1970', 'Taxi Driver'),
+(default,'1970', 'The Godfather'),
+(default,'1970', 'Stalker'),
+(default,'1970', 'Jaws'),
+(default,'1970', 'The Exorcist'),
+(default,'1970', 'A Clockwork Orange'),
+
+(default,'1980', 'Beetlejuice'),
+(default,'1980', 'An American Werewolf in London'),
+(default,'1980', 'The Thing'),
+(default,'1980', 'Scarface'),
+(default,'1980', 'The Color Purple'),
+(default,'1980', 'The Shining'),
+(default,'1980', 'Apocalypse Now'),
+(default,'1980', 'Once Upon a Time in America'),
+
+(default,'1990', 'Malcolm X'),
+(default,'1990', 'Se7en'),
+(default,'1990', 'Heat'),
+(default,'1990', 'The Devils Advocate'),
+(default,'1990', 'Perfect Blue'),
+(default,'1990', 'Misery'),
+(default,'1990', 'Schindlers List'),
+(default,'1990', 'GoodFellas'),
+
+(default,'2000', 'Office Space'),
+(default,'2000', 'No Country for Old Men'),
+(default,'2000', 'Watchmen'),
+(default,'2000', 'Who Am I'),
+(default,'2000', 'The Conjuring'),
+(default,'2000', 'Into the Wild'),
+(default,'2000', 'The Imitation Game'),
+(default,'2000', 'Get Out');
+
+select * from cinema;
--- principalTeste.py
+++ principalTeste.py
@@ -4,7 +4,7 @@ from conexão import CriarConexao, FecharConexao
 
 def InserirFilmes(con,ano,filme):
     cursor = con.cursor()
-    sql = "INSERT INTO filmes (ano, filme) values (%s, %s, %s)"
+    sql = "INSERT INTO filmes (ano, filme) values (%s, %s)"
     valores = (ano, filme)
     cursor.execute(sql, valores)
     cursor.close()
@@ -12,7 +12,7 @@ def InserirFilmes(con,ano,filme):
 
 def MostrarFilmes(con):
     cursor = con.cursor()
-    sql = "SELECT id, ano, filme FROM cinema"
+    sql = "SELECT id, ano, filme FROM biblioteca"
     cursor.execute(sql)
     
     for (id, ano, filme) in cursor:
@@ -21,18 +21,18 @@ def MostrarFilmes(con):
     cursor.close()
 
 def main():
-    con = CriarConexao("localhost", "ano", "filmes",)
+    con = CriarConexao("127.0.0.1", "root", "", "cinema",)
     
-    InserirFilmes(con, "2022", "Tudo em Todo Lugar ao Mesmo Tempo")
+    #InserirFilmes(con, "2022", "Tudo em Todo Lugar ao Mesmo Tempo")
     MostrarFilmes(con)
     
     FecharConexao(con)
 
-with open('arquivos/Filmes.csv','r') as arquivo:
-    leitor = csv.reader(arquivo)
-    ano = list(leitor)
-    ql = len(ano)
-    fl = randrange(1, ql-1)
-    al = randrange(0, 4)
-    
-    print(f'O ano escolhido foi \033[1;32m{ano[0][al]}\033[m e o filme \033[1;36m{ano[fl][al]}\033[m.')
+#with open('arquivos/Filmes.csv','r') as arquivo:
+#    leitor = csv.reader(arquivo)
+#    ano = list(leitor)
+#    ql = len(ano)
+#    fl = randrange(1, ql-1)
+#    al = randrange(0, 4)
+#    
+#    print(f'O ano escolhido foi \033[1;32m{ano[0][al]}\033[m e o filme \033[1;36m{ano[fl][al]}\033[m.')
